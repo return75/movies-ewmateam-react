138b9d14bdc645256f351920b636028b80ba7960
atashfaraz <atashfaraz8@gmail.com>
2022-11-27 16:53:44 +0330

    list movie cards in main page

--- src/components/movies/MoviesCard.js
+++ src/components/movies/MoviesCard.js
@@ -1,6 +1,12 @@
 export default (props) => {
-    return <div>
-        <img src={props.image} alt="image"/>
+    return <div className={'movie-card'}>
+        <div
+            className={'image-cover'}
+            style={{
+                backgroundImage: `url(https://image.tmdb.org/t/p/original/${props.image})`,
+            }}
+        >
+        </div>
         <h5>{props.title}</h5>
         <div>{props.date}</div>
     </div>
--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,13 +1,35 @@
-//import MoviesCard from "../components/movies/MoviesCard";
+import React from "react";
+
+import MoviesCard from "../components/movies/MoviesCard";
 import movieService from "../services/movieService";
 
+class MoviesPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: []
+        }
+    }
+    render() {
+        return (
+            <div className={'d-flex flex-wrap'}>
+                {this.state.movies.map(movie =>
+                    <MoviesCard
+                        image={movie.backdrop_path}
+                        title={movie.title}
+                        date={movie.release_date}>
+                    </MoviesCard>
+                )}
+            </div>
+        )
+    }
+    async componentDidMount() {
 
-async function MoviesPage() {
-    let movies = await movieService.getMoviesList()
-    console.log(movies)
-    return <div>
-       <h1>ali</h1>
-    </div>
+        let movies = await movieService.getMoviesList()
+        console.log('movies', movies)
+        this.setState({movies: movies.data.results})
+
+    }
 }
 
 export default MoviesPage
